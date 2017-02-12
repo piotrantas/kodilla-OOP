1f8af99c8bc110de5c77e8de659cb50768737775
Piotr Antas <antas.piotr@gmail.com>
2017-02-12 17:49:40 +0100

    comments delete

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
-function Button(text) { // funkcja tworząca klasę button z parametrem text
-	this.text = text || 'Hello'; // klasa z jednym parametrem o wartości text lub (||) jęsli nie podano parametru w konstrutorze wartość 'Hello'
+function Button(text) { 
+	this.text = text || 'Hello'; 
 }
 
-Button.prototype = { //prototyp, mechanizm, który będzie coś robił z stowrzonym przyciskiem, w tym przypadku m.in wyświetlał na stronie
-	create: function() { // metoda (jQuery) tworząca
-		var self = this; // zmienna self ]NIE DO KOŃCA KUMAM O CO CHODZI]. Mówimy, że self to to samo co this?
-		this.$element = $( '<button>' ); //tworzymy button (poprzez jQuery)
-		this.$element.text(this.text); // dodajemy do button text (ten który był parametrem stworzonego obiektu)
-		this.$element.click(function() { // ustawiamy clik na buttonie, który wykona funkcję alertującą
-			alert(self.text); // alertujemy/wyświetlamy w wyskakującym oknie tekst - odwołujemy się tu do self, które, jak wcześniej określiliśmy jest tym samym co this. Musimy tak zrobić, bo mamy tu funkcję w funkcji i js gubi kontekst, w ten sposób mu go wskazujemy
+Button.prototype = { 
+	create: function() { 
+		var self = this; 
+		this.$element = $( '<button>' ); 
+		this.$element.text(this.text); 
+		this.$element.click(function() { 
+			alert(self.text); 
 		});
-		$('body').append(this.$element); // dodajemy (przez jQuery) tak stworzony button
+		$('body').append(this.$element); 
 	}
 }
-var btn1 = new Button('Hello!'); //konstruktor buttona poprzez instrukcję new z parametrem 'Hello!'. Gdybyśmy go nie podali, to wyświetliłby to co po lub(||) w cunkcji klasy
-btn1.create(); //wywołanie tworzenia butona na podstawie danych ze zmiennej btn1
+var btn1 = new Button('Hello!'); 
+btn1.create(); ś
